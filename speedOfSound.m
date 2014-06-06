0ac5caa644e0420aca738fc343cc0d6c1c3071ff
Neil Ryan <soctar@gmail.com>
2014-06-06 15:09:19 -0400

    Removed unused function
    removed "lagrange" - never used.

--- speedOfSound.m
+++ speedOfSound.m
@@ -24,15 +24,3 @@ elseif strcmp(units, 'kph')
 else
     error('Incorrect units!');
 end
-
-function h = lagrange(N, delay)
-%LAGRANGE  h=lagrange(N,delay) returns order N FIR 
-%          filter h which implements given delay 
-%          (in samples).  For best results, 
-%          delay should be near N/2 +/- 1.
-n = 0:N;
-h = ones(1,N+1);
-for k = 0:N
-    index = find(n ~= k);
-    h(index) = h(index) *  (delay-k)./ (n(index)-k);
-end
