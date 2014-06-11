e072b097e42721074d55fa6c1944f985f04e7cae
soctar <soctar@gmail.com>
2014-06-11 09:49:18 -0400

    More debugging 6/11
    Phi goes from -90 to 90 now. Changed 'last' settings.

--- beamforming_2d.m
+++ beamforming_2d.m
@@ -1,24 +1,23 @@
+clear all
+close all
 sim mic_input_2d
 microphones = [1 2; 3 4];
-signals = [mic1,mic2,mic3,mic4];
+signals = [mic1,mic3,mic2,mic4];
 num_mics = 4;
 micSep = .1;
 deg_res = 10; %Change to change resolution, MUST use factors of 90,180
-sampRate = 44100;
+sampR = 44100; %sampling rate
 speed = 345; %speed of sound
 power_lvls = zeros(floor(180/deg_res)+1, floor(180/deg_res)+1);
-%disp(numel(power_lvls))
-for phi = deg_res:deg_res:90-deg_res
+assert(90/deg_res == floor(90/deg_res))
+
+for phi = -90+deg_res:deg_res:0-deg_res%excludes phi=-90
     for theta = deg_res:deg_res:180-deg_res
-        %disp('phi'),disp(phi)
-        %disp('theta'),disp(theta)
         signalSum = zeros(1,length(mic1));
         if theta<90 %Cannot be 0
-            %disp('less')
             mics = microphones;%mics is the mics of interest
             last = mics(end,1);
         elseif theta>90 %Cannot be 180
-            %disp('more')
             mics = microphones;
             last = mics(1,1);
         elseif theta == 90
@@ -26,25 +25,25 @@ for phi = deg_res:deg_res:90-deg_res
             last = mics(1);
         end
         for mic = 1:numel(mics)%number of mics of interest
-            if mic~=last
-                %disp('mic'),disp(mic),disp('last'),disp(last)
-                delay=calcDelay2d(phi,theta,mics,mic,last,sampRate,speed,micSep);
-                %disp('delay'),disp(delay)
+            if mic~=last%pb - when mics~=microphones might miss index
+                delay=calcDelay2d(phi,theta,mics,mic,last,sampR,speed,micSep);
                 hdint = dfilt.delay(floor(delay));
                 dfrac = fdesign.fracdelay(delay-floor(delay));
                 hdfrac = design(dfrac,'lagrange','filterstructure','farrowfd');
                 micDelayed=filter(hdfrac,filter(hdint,signals(1:end,mic)));
             else
-                %disp('no')
                 micDelayed=signals(1:end,mic);
             end
             for i = 1:length(mic1)%sums mic signals
                 signalSum(i) = signalSum(i) + micDelayed(i);
             end
-            power_lvls(phi/deg_res,theta/deg_res)=bandpower(signalSum,sampRate,[200,20000]);
+            pind = (phi+90)/deg_res;%phi adjusted to index into array
+            tind = theta/deg_res; %theta adj. to index into array
+            power_lvls(pind,tind)=bandpower(signalSum,sampR,[200,20000]);
         end
     end
 end
+
 disp('half')
 signalSum = zeros(1,length(mic1));
 for mic=1:num_mics %case where phi=90
@@ -52,23 +51,24 @@ for mic=1:num_mics %case where phi=90
         signalSum(i) = signalSum(i)+signals(i,mic);
     end
 end
-power_lvls(90/deg_res,1) = bandpower(signalSum,sampRate,[200,20000]);
-for phi = 90+deg_res:deg_res:180-deg_res
-    for theta = deg_res:deg_res:180-deg_res
+power_lvls(90/deg_res,1) = bandpower(signalSum,sampR,[200,20000]);
+
+for phi = 0+deg_res:deg_res:90-deg_res%excludes phi=90
+    for theta = deg_res:deg_res:180-deg_res%excludes theta=0,180
         signalSum = zeros(1,length(mic1));
         if theta<90 %Cannot be 0
             mics = microphones;%mics is the mics of interest
-            last = mics(end,end);
+            last = mics(1,end);
         elseif theta>90 %Cannot be 180
             mics = microphones;
-            last = mics(1,end);
+            last = mics(end,end);
         elseif theta == 90
             mics = microphones(1,:);
             last = mics(end);
         end
         for mic = 1:numel(mics)%number of mics of interest
             if mic~=last
-                delay=calcDelay2d(phi,theta,mics,mic,last,sampRate,speed,micSep);
+                delay=calcDelay2d(phi,theta,mics,mic,last,sampR,speed,micSep);
                 hdint = dfilt.delay(floor(delay));
                 dfrac = fdesign.fracdelay(delay-floor(delay));
                 hdfrac = design(dfrac,'lagrange','filterstructure','farrowfd');
@@ -79,7 +79,9 @@ for phi = 90+deg_res:deg_res:180-deg_res
             for i = 1:length(mic1)%sums mic signals
                 signalSum(i) = signalSum(i) + micDelayed(i);
             end
-            power_lvls(phi/deg_res,theta/deg_res)=bandpower(signalSum,sampRate,[200,20000]);
+            pind = (phi+90)/deg_res;%phi adjusted to index into array
+            tind = theta/deg_res; %theta adj. to index into array
+            power_lvls(pind,tind)=bandpower(signalSum,sampR,[200,20000]);
         end
     end
 end
--- calcDelay2d.m
+++ calcDelay2d.m
@@ -1,13 +1,16 @@
-function [numSamples] = calcDelay2d(phi,theta,mics,mic1,mic2,sampRate,speed,micSep)
-phi_rads = degtorad(phi-90);%to give correct sine values?
-theta_rads = degtorad(theta);
-sampTime = 1/sampRate;
+function [numSamples] = calcDelay2d(p,t,mics,mic1,mic2,sampR,speed,micSep)
+%p-phi, t-theta, sampR-sampling rate
+phi_rads = degtorad(p);
+theta_rads = degtorad(t);
+sampTime = 1/sampR;
 [row1,col1] = find(mics == mic1);
 [row2,col2] = find(mics == mic2);
-if phi~=90
+if p~=0
     if (row1==row2) && (col1~=col2)
         [dist,angle] = findDist(row1,col1,row2,col2,micSep);
+        %disp(dist),disp(angle)
         v = speed*sin(phi_rads)*sin(theta_rads+angle);
+        %disp(v)
         time = dist/v;
         numSamples = abs(time/sampTime);
     elseif (row1~=row2) && (col1~=col2)
