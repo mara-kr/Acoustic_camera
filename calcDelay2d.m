36dde6e5005eed1feaf539171697882fee6181c3
soctar <soctar@gmail.com>
2014-06-11 12:15:58 -0400

    New delay formula-should be correct

--- calcDelay2d.m
+++ calcDelay2d.m
@@ -7,21 +7,19 @@ sampTime = 1/sampR;
 [row2,col2] = find(mics == mic2);
 if p~=0
     if (row1==row2) && (col1~=col2)
-        [dist,angle] = findDist(row1,col1,row2,col2,micSep);
-        %disp(dist),disp(angle)
-        v = speed*sin(phi_rads)*sin(theta_rads+angle);
-        %disp(v)
-        time = dist/v;
+        dist = findDist(row1,col1,row2,col2,micSep);
+        distance = dist*sin(theta_rads)*sin(phi_rads);
+        time = distance/speed;
         numSamples = abs(time/sampTime);
     elseif (row1~=row2) && (col1~=col2)
-        [dist,angle] = findDist(row1,col1,row2,col2,micSep);
-        v = speed*sin(phi_rads)*sin(theta_rads+angle);
-        time = dist/v;
+        dist = findDist(row1,col1,row2,col2,micSep);
+        distance = dist*sin(phi_rads)*(theta_rads);
+        time = distance/speed;
         numSamples = abs(time/sampTime);
     elseif (row1~=row2) && (col1==col2)
-        [dist,angle] = findDist(row1,col1,row2,col2,micSep); %#ok<NASGU>
-        v = speed*sin(phi_rads)*cos(theta_rads);
-        time = dist/v;
+        dist = findDist(row1,col1,row2,col2,micSep);
+        distance = dist*sin(phi_rads)*cos(theta_rads);
+        time = distance/speed;
         numSamples = abs(time/sampTime);
     else
         disp('PROBLEM!!!')
